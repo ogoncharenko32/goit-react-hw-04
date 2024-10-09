4462f95b952b875454bf7b27c3e8532f4693983b
ogoncharenko32 <krapniknil32@gmail.com>
2024-10-09 20:47:16 +0300

    fix

--- src/Components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/Components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,6 +1,7 @@
 import css from "./LoadMoreBtn.module.css"
 
 const LoadMoreBtn = ({ onLoadMore }) => {
+  
   return (
     <button className={css.btn} onClick={onLoadMore} type='button'>Load More</button>
   )
